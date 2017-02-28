6c24eea099b7afac0270c1173c62b049cc4d28c6
loladin123 <inflames92@hotmail.se>
2017-02-28 19:49:38 +0100

    E3 - Bitbuffer finished

--- E3 - Bitbuffer/Bitbuffertest.cpp
+++ E3 - Bitbuffer/Bitbuffertest.cpp
@@ -7,38 +7,40 @@ using namespace std;
 
 int main() {
 	
-	unsigned char test = 10;
-	unsigned char test2 = 24;
-	unsigned char test3, test4;
+	/* Just some testing of bit-wise operators using bitset library to display them as
+	binary instead of decimal */
+	//unsigned char test = 1;
+	//unsigned char test2 = 24;
+	//unsigned char test3, test4;
+	//
+	//bitset<8> a(test);
+	//bitset<8> b(test2);
+	//cout << "A:  " << a << endl;
+	//cout << "B:  " << b << endl;
+	//cout << "&:  " << (a & b) << endl;
+	//cout << "^:  " << (a ^ b) << endl;
+	//cout << "|:  " << (a | b) << endl;
+	//
+	//test3 = (test << 1);
+	//bitset<8> c(test3);
+	//cout << "<<: " << c << endl;
 
+	//test4 = (test2 >> 1);
+	//bitset<8> d(test4);
+	//cout << ">>: " << d << endl;
 
-	bitset<8> a(test);
-	bitset<8> b(test2);
-	cout << "A:  " << a << endl;
-	cout << "B:  " << b << endl;
-	cout << "&:  " << (a & b) << endl;
-	cout << "^:  " << (a ^ b) << endl;
-	cout << "|:  " << (a | b) << endl;
-	
-	test3 = (test << 1);
-	bitset<8> c(test3);
-	cout << "<<: " << c << endl;
+	//int k = 0;
+	//bool b = true;
+	//b = 12;
+	//cout << b; 
+	//
 
-	test4 = (test2 >> 1);
-	bitset<8> d(test4);
-	cout << ">>: " << d << endl;
-
-	
-	
-	
-	
-	
-	
-	
-	//BitBuffer buf(cout);
-	//int n;
-	//while (cin >> n) {
-	//	buf.put(n != 0);
-	//}
-	//cout << endl;
+	/* Actual task. The cout argument is passed so that every object has its
+	own output stream. n is converted to 1 for all ints > 0 and 0 for 0 */
+	BitBuffer buf(cout);
+	int n;
+	while (cin >> n) {
+		buf.put(n);
+	}
+	cout << endl;
 }
--- E3 - Bitbuffer/bitbuffer.cc
+++ E3 - Bitbuffer/bitbuffer.cc
@@ -1,8 +1,18 @@
 #include "stdafx.h"
 #include "bitbuffer.h"
 #include <iostream>
+#include <cassert>
+#include <bitset>
 
-BitBuffer::BitBuffer(std::ostream& out) {}
+BitBuffer::BitBuffer(std::ostream& out) : os(out), n_bits(0) { }
 
 void BitBuffer::put(bool b) {
+	
+	/* Pushes bitwise to the left <-- to "make room" for new bits. os.put converts to char
+	and outputs. */
+	byte = (byte << 1) | b;
+	if (n_bits == 7)
+	os.put(byte), n_bits = 0;
+	else
+		n_bits++;
 }
