e016d7faff52e796c13e29044ddd6436c3038f52
loladin123 <inflames92@hotmail.se>
2017-03-04 15:02:49 +0100

    E4 - Rational finished

--- E4 - Rationaltest/Rational.cpp
+++ E4 - Rationaltest/Rational.cpp
@@ -1,17 +1,15 @@
 #include "stdafx.h"
 #include "Rational.h"
 
-
+/* Private gcd function */
 int Rational::gcd(int n, int d) {
 	return d == 0 ? n : gcd(d, n % d);
 }
-
+/* Private function that finds gcd and makes de nom/denom small as possible */
 void Rational::fix(int &n, int &d) {
 	int gcd1 = gcd(n, d);
 	if (gcd1 != 1)
 		n = (n / gcd1), d = (d / gcd1);
-
-
 }
 
 Rational& Rational::operator=(const int a) {
@@ -20,35 +18,18 @@ Rational& Rational::operator=(const int a) {
 	return *this;
 }
 
-//Rational &Rational::operator+=(const Rational &other){
-//	
-//}
-
+Rational &Rational::operator+=(const Rational &other){
+	return *this = *this + other;
+}
+/* No '&' here because if a = b + c, we don't want to change b we only want to copy it */
 Rational Rational::operator+(const Rational &other) {
 	Rational rat;
-	if (rat.denominator == other.denominator) {
-		rat.nominator = this->nominator + other.nominator;
-		fix(rat.nominator, rat.denominator);
-		return rat;
-	}
-	if (rat.denominator == 1) {
-		rat.nominator = (this->nominator * other.denominator) + other.nominator;
-		fix(rat.nominator, rat.denominator);
-		return rat;
-	}
-	if (other.denominator == 1) {
-		rat.nominator = (this->denominator * other.nominator) + this->nominator;
-		fix(rat.nominator, rat.denominator);
-		return rat;
-	}
-	if ((rat.denominator != other.denominator)) {
-		int newnominator = (this->nominator * other.denominator) + (other.nominator * this->denominator);
-		int newdenominator = this->denominator * other.denominator;
-		rat.nominator = newnominator;
-		rat.denominator = newdenominator;
-		fix(rat.nominator, rat.denominator);
-		return rat;
-	}
+	rat.nominator = this->nominator;
+	rat.denominator = this->denominator;
+	rat.nominator = (rat.nominator * other.denominator) + (other.nominator * rat.denominator);
+	rat.denominator = rat.denominator * other.denominator;
+	fix(rat.nominator, rat.denominator);
+	return rat;
 }
 
 Rational::~Rational()
--- E4 - Rationaltest/Rational.h
+++ E4 - Rationaltest/Rational.h
@@ -1,28 +1,27 @@
 #pragma once
 #include <ostream>
 using std::ostream;
-class Rational
-{
+class Rational {
 public:
-	Rational(int n = 0, int d = 1) : nominator(n), denominator(d) {};
-	Rational(const Rational& other) : nominator(other.nominator), denominator(other.denominator) {};
+	Rational(int n = 0, int d = 1) : nominator(n), denominator(d) { fix(nominator, denominator);  };
 	friend ostream& operator<<(ostream &os, Rational& r);
 	Rational& operator=(const int a);
 	Rational& operator+=(const Rational &other);
 	Rational operator+(const Rational &other);
 	~Rational();
-	int getN() { return nominator;  }
-	int getD() { return denominator; }
+private:
 	void fix(int &n, int &d);
 	int gcd(int n, int d);
-private:
-
 	int nominator, denominator;
 	
 };
 /* the reason we don't define ostream operator is because it "comes from a different library"
-and it causes some weird shit */
+and it causes some weird shit - I think */
 inline ostream& operator<<(ostream &os, Rational &r) {
-
+	if (r.denominator == 1)
+		os << r.nominator;
+	else
+		os << r.nominator << '/' << r.denominator;
+	return os;
 }
 
